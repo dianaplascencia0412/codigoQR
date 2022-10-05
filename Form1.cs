dbb5da4e950fc42e30b61873044e6a59aff45605
dianaplascencia0412 <114034425+dianaplascencia0412@users.noreply.github.com>
2022-10-05 11:51:07 -0500

    Comit nuevo

--- Form1.cs
+++ Form1.cs
@@ -16,5 +16,10 @@ namespace codigoQR
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            label1.Text = "Hola 2";
+        }
     }
 }
